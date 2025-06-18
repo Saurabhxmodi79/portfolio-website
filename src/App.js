8526dc60d2b899b0991b3031993bbfd39467ce14
Modi Saurabh <Modi.saurabh@csgi.com>
2025-06-19 00:16:07 +0530

    Enable Experience section: uncomment Experience component and add navigation link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-// import Experience from "./components/Experience";
+import Experience from "./components/Experience";
 import Education from "./components/Education";
 import ProjectDetails from "./components/ProjectDetails";
 import styled from "styled-components";
@@ -38,7 +38,7 @@ function App() {
           <HeroSection />
           <Wrapper>
             <Skills />
-            {/* <Experience /> */}
+            <Experience />
           </Wrapper>
           <Projects openModal={openModal} setOpenModal={setOpenModal} />
           <Wrapper>
