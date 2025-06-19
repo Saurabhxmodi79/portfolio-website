94fd5fa262dbd2d5c98d638cf728160f067a9498
Modi Saurabh <Modi.saurabh@csgi.com>
2025-06-19 22:18:51 +0530

    making changes for data constants

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -92,7 +92,7 @@ function Footer() {
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
-          {/* <NavLink href="#experience">Experience</NavLink> */}
+          <NavLink href="#experience">Experience</NavLink>
           <NavLink href="#projects">Projects</NavLink>
           <NavLink href="#education">Education</NavLink>
         </Nav>
